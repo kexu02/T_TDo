89c91ae85efa8c3b37d5b1cb430c18b5bd2b6d84
kexu02 <k92xu@uwaterloo.ca>
2020-12-29 17:20:12 -0800

    added homepage

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const ejs = require("ejs");
 
 const app = express();
 
 app.use(express.static("Public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+// homepage
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/signIn.html");
 })
 
 app.post("/", function(req, res) {
-
+    res.sendFile(__dirname + "/homepage.html");
 })
 
+// connects to webpage
 app.listen(process.env.PORT || 3000, function() {
 
 })
