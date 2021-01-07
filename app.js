bc10a06a37ddabb855f60c5ebf24ab580e5033af
Emma-He <emma.xiaotong@gmail.com>
2021-01-07 14:28:08 -0400

    asdf

--- app.js
+++ app.js
@@ -124,21 +124,21 @@ app.get("/signIn", function(req, res) {
 });
 
 app.post("/signIn", function(req, res) {
-  const user = new User({
-    username: req.body.username,
-    password: req.body.password
-  });
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
 
-  req.login(user, function(err) {
-    if (!err) {
-      passport.authenticate("local")(req, res, function() {
-        res.redirect("/");
-      })
-    } else {
-      res.redirect("/signIn");
-      failureFlash: 'Invalid username or password.';
-    }
-  });
+    req.login(user, function(err) {
+        if (!err) {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/");
+            })
+        } else {
+            res.redirect("/signIn");
+            failureFlash: 'Invalid username or password.';
+        }
+    });
 });
 
 // sign up page
@@ -164,57 +164,36 @@ app.post("/signUp", function(req, res) {
 
 // calender
 app.get("/cal", function(req, res) {
-<<<<<<< HEAD
-    res.render("cal");
-    Task.find({}, function(err, foundItems) {
-        res.render("cal", {
-            taskList: foundItems
+    if (req.isAuthenticated()) {
+        Task.find({}, function(err, foundItems) {
+            res.render("cal", { taskList: foundItems });
         });
-    });
-=======
-  if (req.isAuthenticated()) {
-     Task.find({}, function(err, foundItems) {
-     res.render("cal", { taskList: foundItems });
-  });
-  } else {
-    res.redirect("/signIn");
-  }
->>>>>>> 3e706c4ea9b6967905630272b893cc4a56a887fa
+    } else {
+        res.redirect("/signIn");
+    }
 });
 
 // to do list
 app.get("/list", function(req, res) {
-  if (req.isAuthenticated()) {
-      Task.find({}, function(err, foundItems) {
-      res.render("list", { newListItems: foundItems });
-    });
-  } else {
-    res.redirect("/signIn");
-  }
+    if (req.isAuthenticated()) {
+        Task.find({}, function(err, foundItems) {
+            res.render("list", { newListItems: foundItems });
+        });
+    } else {
+        res.redirect("/signIn");
+    }
 });
 
 app.post("/list", function(req, res) {
-<<<<<<< HEAD
+    console.log(req.User);
     const taskItem = req.body.newItem;
     const task = new Task({
-        // username: req.user.username,
+        // username: req.User.username,
         item: taskItem
     });
     task.save();
     res.redirect("/list");
 });
-
-=======
-  console.log(req.User);
-  const taskItem = req.body.newItem;
-  const task = new Task({
-    // username: req.User.username,
-    item: taskItem
-  });
-  task.save();
-  res.redirect("/list");
-});
->>>>>>> 3e706c4ea9b6967905630272b893cc4a56a887fa
 
 // log out
 app.get("/logout", function(req, res) {
