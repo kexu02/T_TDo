476399fc81eb67fe0749b7d7a8ba3a5c1457cd0c
Emma-He <emma.xiaotong@gmail.com>
2021-01-07 16:06:29 -0400

    test

--- app.js
+++ app.js
@@ -168,6 +168,9 @@ app.get("/cal", function(req, res) {
         Task.find({}, function(err, foundItems) {
             res.render("cal", { taskList: foundItems });
         });
+        users.find({}, function(err, usernames) {
+            res.render("cal", { nameList: usernames });
+        });
     } else {
         res.redirect("/signIn");
     }
